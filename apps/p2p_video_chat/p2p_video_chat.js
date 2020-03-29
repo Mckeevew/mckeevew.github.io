f6ddca43374a16b4345a7fa4c69e98cab7c0c648
mckeevew <williamscottmckeever@gmail.com>
2020-03-29 11:04:05 -0400

    backup

--- apps/p2p_video_chat/p2p_video_chat.js
+++ apps/p2p_video_chat/p2p_video_chat.js
@@ -1,27 +1,44 @@
-var Peer = require('simple-peer')
-var peer = new Peer({
-    initiator: location.hash === '#init',
-    trickle: false
-})
 
-peer.on('signal', function(data)
+navigator.webkitGetUserMedia({video: true, audio: true}, function(stream)
 {
-    document.getElementById('yourId').value = JSON.stringify(data)
-})
+    var Peer = require('simple-peer')
+    var peer = new Peer({
+        initiator: location.hash === '#init',
+        trickle: false,
+        stream: stream
+    })
 
-document.getElementById('connect').addEventListener('click', function()
-{
-    var otherId = JSON.parse(document.getElementById('otherID').value)
-    peer.signal(otherID)
-})
+    peer.on('signal', function(data)
+    {
+        document.getElementById('yourId').value = JSON.stringify(data)
+    })
 
-document.getElementById('send').addEventListener('click', function() 
-{
-    var yourMessage = document.getElementById('yourMessage').value
-    peer.send(yourMessage)
-})
+    document.getElementById('connect').addEventListener('click', function()
+    {
+        var otherId = JSON.parse(document.getElementById('otherID').value)
+        peer.signal(otherID)
+    })
 
-peer.on('data', function(data)
+    document.getElementById('send').addEventListener('click', function() 
+    {
+        var yourMessage = document.getElementById('yourMessage').value
+        peer.send(yourMessage)
+    })
+
+    peer.on('data', function(data)
+    {
+        document.getElementById('messages').textContent += data + '\n'
+    })
+
+    peer.on('stream', function(stream)
+    {
+        var video = document.createElement('video')
+        document.getElementById(video-wrapper).appendChild(video)
+
+        video.src = window.URL.createObjectURL(stream)
+        video.play()
+    })
+}, function(err)
 {
-    document.getElementById('messages').textContent += data + '\n'
+    console.error(err)
 })
