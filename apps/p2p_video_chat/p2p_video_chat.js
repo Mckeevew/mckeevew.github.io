fc2c9b788f6bab244ec210711719b43520ed44b3
mckeevew <williamscottmckeever@gmail.com>
2020-03-29 10:18:31 -0400

    Added apps and Video chat.

---
+++ apps/p2p_video_chat/p2p_video_chat.js
@@ -0,0 +1,27 @@
+var Peer = require('simple-peer')
+var peer = new Peer({
+    initiator: location.hash === '#init',
+    trickle: false
+})
+
+peer.on('signal', function(data)
+{
+    document.getElementById('yourId').value = JSON.stringify(data)
+})
+
+document.getElementById('connect').addEventListener('click', function()
+{
+    var otherId = JSON.parse(document.getElementById('otherID').value)
+    peer.signal(otherID)
+})
+
+document.getElementById('send').addEventListener('click', function() 
+{
+    var yourMessage = document.getElementById('yourMessage').value
+    peer.send(yourMessage)
+})
+
+peer.on('data', function(data)
+{
+    document.getElementById('messages').textContent += data + '\n'
+})
